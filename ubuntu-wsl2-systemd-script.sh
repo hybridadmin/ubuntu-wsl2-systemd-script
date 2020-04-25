60ee7595c3388eb1aae1c5715c9d2a4de46be242
hybridadmin <46610714+hybridadmin@users.noreply.github.com>
2020-04-25 19:15:59 +0200

    Update ubuntu-wsl2-systemd-script.sh

--- ubuntu-wsl2-systemd-script.sh
+++ ubuntu-wsl2-systemd-script.sh
@@ -3,10 +3,12 @@ if [[ "$EUID" -ne 0 ]]; then
   echo "Please run as root/sudo"
   exit
 fi
+
 apt-get update && sudo apt-get install -yqq daemonize dbus-user-session fontconfig
 cp ./start-systemd-namespace /usr/sbin/.
 cp ./enter-systemd-namespace /usr/sbin/.
 chmod +x /usr/sbin/enter-systemd-namespace
+
 echo 'Defaults        env_keep += WSLPATH' | EDITOR='tee -a' visudo
 echo 'Defaults        env_keep += WSLENV' | EDITOR='tee -a' visudo
 echo 'Defaults        env_keep += WSL_INTEROP' | EDITOR='tee -a' visudo
@@ -22,3 +24,6 @@ else
 fi
 
 sudo sed -i 2a"# Start or enter a PID namespace in WSL2\nsource /usr/sbin/start-systemd-namespace\n" ${CONFIG_FILE}
+
+cmd.exe /C setx WSLENV BASH_ENV/u
+cmd.exe /C setx BASH_ENV ${CONFIG_FILE}
