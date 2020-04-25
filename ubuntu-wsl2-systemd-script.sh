40c7a48330aa3e51c47a89fade6e09294955e603
hybridadmin <46610714+hybridadmin@users.noreply.github.com>
2020-04-25 19:12:46 +0200

    Update ubuntu-wsl2-systemd-script.sh

--- ubuntu-wsl2-systemd-script.sh
+++ ubuntu-wsl2-systemd-script.sh
@@ -13,4 +13,12 @@ echo 'Defaults        env_keep += WSL_INTEROP' | EDITOR='tee -a' visudo
 echo 'Defaults        env_keep += WSL_DISTRO_NAME' | EDITOR='tee -a' visudo
 echo 'Defaults        env_keep += PRE_NAMESPACE_PATH' | EDITOR='tee -a' visudo
 echo '%sudo ALL=(ALL) NOPASSWD: /usr/sbin/enter-systemd-namespace' | EDITOR='tee -a' visudo
-sudo sed -i 2a"# Start or enter a PID namespace in WSL2\nsource /usr/sbin/start-systemd-namespace\n" /etc/bash.bashrc
+
+DEFAULT_SHELL=$(cat /etc/passwd | grep $(whoami) | cut -d ':' -f7 | cut -d '/' -f3)
+if [ "${DEFAULT_SHELL}" == "zsh" ]; then
+        CONFIG_FILE="/etc/zsh/zshrc"
+else
+        CONFIG_FILE="/etc/bash.bashrc"
+fi
+
+sudo sed -i 2a"# Start or enter a PID namespace in WSL2\nsource /usr/sbin/start-systemd-namespace\n" ${CONFIG_FILE}
